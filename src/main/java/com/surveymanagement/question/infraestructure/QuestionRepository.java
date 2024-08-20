18a9ad98a0e6a352b3f95e4a5c3412947e5ad0a5
simonmantillago <150193274+simonmantillago@users.noreply.github.com>
2024-08-20 00:48:03 -0500

    perf: :lipstick: Update UserRolesUi for better performance

--- src/main/java/com/surveymanagement/question/infraestructure/QuestionRepository.java
+++ src/main/java/com/surveymanagement/question/infraestructure/QuestionRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 
-import com.surveymanagement.chapter.domain.entity.Chapter;
 import com.surveymanagement.question.domain.entity.Question;
 import com.surveymanagement.question.domain.service.QuestionService;
 
